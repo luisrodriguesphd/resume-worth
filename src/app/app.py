3cd0212bdc47f5509b2087858b30f70014836ba4
Luis Rodrigues <luisrodrigues@Luiss-MacBook-Pro.local>
2024-04-07 12:20:09 +0100

    adds notebook to scrap jobs from job street

--- src/app/app.py
+++ src/app/app.py
@@ -4,15 +4,19 @@ from resume_worth.pipelines.information_retrieval.pipeline import retrieve_top_j
 
 
 params = get_params()
-APP_TITLE = params['APP_TITLE']
-APP_DESCRIPTION = params['APP_DESCRIPTION']
-JOB_TITLES = params['JOB_TITLES']
-HOST = params['HOST']
-PORT = params['PORT']
+app_config = params['app_config']
+app_backend = params['app_backend']
+app_frontend = params['app_frontend']
 
 
-def salary_estimator(job_title, resume):
+def salary_estimator(job_title: str, resume: str):
 
+    if job_title is None:
+        return app_frontend['messages']['job_title_not_found']
+
+    if len(resume) < app_backend['min_resume_size']:
+        return app_frontend['messages']['salary_not_found']
+    
     salaries = retrieve_top_job_vacancy_info(job_title, resume)
 
     return salaries[0]
@@ -23,19 +27,19 @@ def run():
     app = gr.Interface(
         fn=salary_estimator, 
         inputs=[
-            gr.Radio(JOB_TITLES, label="Job Title"),
+            gr.Radio(app_frontend['jobs'], label="Job Title"),
             gr.Textbox(label="Resume", lines=10)
         ],
         outputs=[
             gr.Textbox(label="Estimate Salary", lines=1)
         ],
-        title=APP_TITLE,
-        description=APP_DESCRIPTION,
+        title=app_frontend['title'],
+        description=app_frontend['description'],
         allow_flagging="never"
     )
 
     # Use share=True to create a public link to share. This share link expires in 72 hours.
-    app.launch(server_name=HOST, server_port=int(PORT))
+    app.launch(server_name=app_config['host'], server_port=app_config['port'])
 
 
 if __name__ == "__main__":
