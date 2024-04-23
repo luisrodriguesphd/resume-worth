44d9425d84a390ce0bdd1a51e5206b7e4e055383
Luis Rodrigues <larpig@gmail.com>
2024-04-23 14:58:22 +0100

    feat: add a flag in the UI to enable the explanation feature

--- src/app/app.py
+++ src/app/app.py
@@ -11,7 +11,7 @@ app_backend = params['app_backend']
 app_frontend = params['app_frontend']
 
 
-def salary_estimator(job_title: str, resume_path: str):
+def salary_estimator(sources:str, job_title: str, resume_path: str, explanation_flag: bool):
 
     salary_range, job_url, explanation = '', '', ''
 
@@ -37,7 +37,7 @@ def salary_estimator(job_title: str, resume_path: str):
         if job_url is None:
             job_url = app_frontend['messages']['active_job_not_found']
         
-        if job_description is not None:
+        if (explanation_flag) and (job_description is not None):
             explanation = generate_explanation_why_resume_for_a_job(resume, job_description)
 
     return salary_range, job_url, explanation
@@ -47,18 +47,22 @@ def salary_estimator(job_title: str, resume_path: str):
 demo = gr.Interface(
     fn=salary_estimator, 
     inputs=[
-        gr.Radio(app_frontend['jobs'], label="Job Title"),
-        gr.File(file_count='single', file_types=['.pdf'], label="Resume PDF File", show_label=True,),
+        gr.Radio(app_frontend['sources'], value=app_frontend['sources'][0], label="Sources", info="More sources will be added later!"),
+        gr.Dropdown(app_frontend['jobs'], label="Job Title", info="More jobs will be added later!"),
+        gr.File(file_count='single', file_types=['.pdf'], label="Resume PDF File", show_label=True),
+        gr.Checkbox(value=False, label="Enable the resume-job match explanation feature", info="It takes approximately 5 minutes to run if enabled")
     ],
     outputs=[
         gr.Textbox(label="Salary Range Estimation", lines=1),
-        gr.Textbox(label="Suitable Job Vacancy Sample", lines=1),
-        gr.Textbox(label="Why the Resume and the Job Match", lines=10),
+        gr.Textbox(label="Suitable Job Vacancy Sample", lines=3),
+        gr.Textbox(label="Why the Resume and the Job Match", lines=16),
     ],
     examples=[
         [
+            app_frontend['examples'][1]["source"], 
             app_frontend['examples'][1]["job_title"], 
-            os.path.join(*app_frontend['examples'][1]["resume_path"])
+            os.path.join(*app_frontend['examples'][1]["resume_path"]),
+            app_frontend['examples'][1]["explanation_flag"], 
         ],
     ],
     title=app_frontend['title'],
